a05be6cabede50268e667d85b7aec4fbf286734f
fredluggard <fredrick.ebuka33@gmail.com>
2022-10-27 19:31:04 +0100

    Second Commit

--- Project/musicapp/models.py
+++ Project/musicapp/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+class Artiste(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    songs = models.ForeignKey(Song, on_delete=models.CASCADE) 
+    
+    def __str__(self):
+        return self.name
+
+class Song(models.Model):
+    title = models.CharField(max_length=100)
+    date_released = models.DateField()
+    likes = models.IntegerField()
+    artiste_id = models.ForeignKey(Artiste, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.name
+
+class Lyric(models.Model):
+    content = models.CharField(max_length=100)
+    song_id = models.ForeignKey(Song, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.name
+
--- Project/musicapp/views.py
+++ Project/musicapp/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.https import HttpResponse
 
 # Create your views here.
+def hello(request):
+    return HttpResponse('Hey World')
